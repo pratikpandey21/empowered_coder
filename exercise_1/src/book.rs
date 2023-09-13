c73d09c0d9ba038567901953949bd42f59626b01
pratikpandey <pratik.pandey2111@gmail.com>
2023-09-14 00:18:32 +0200

    feat: set up library management system

--- exercise_1/src/book.rs
+++ exercise_1/src/book.rs
@@ -1,23 +1,29 @@
-pub struct Book {
-    pub title: String,
-    pub author: String,
+use crate::person::Person;
+
+pub struct Book<'a> {
+    pub title: &'a str,
+    pub author: &'a str,
     pub is_available: bool,
+    pub borrowed_by: Option<&'a Person<'a>>,
 }
 
-impl Book {
-    pub fn new(title: String, author: String) -> Self {
-        Self {
+impl<'a> Book<'a> {
+    pub fn new(title: &'a str, author: &'a str) -> Book<'a> {
+        Book {
             title,
             author,
             is_available: true,
+            borrowed_by: None,
         }
     }
 
-    pub fn borrow_book(&mut self) {
+    pub fn borrow_book(&mut self, person: &'a Person<'a>) {
         self.is_available = false;
+        self.borrowed_by = Option::from(person);
     }
 
     pub fn return_book(&mut self) {
         self.is_available = true;
+        self.borrowed_by = None;
     }
 }
--- exercise_1/src/main.rs
+++ exercise_1/src/main.rs
@@ -7,27 +7,21 @@ mod library;
 mod person;
 
 fn main() {
-    let person = Person::new("Pratik".to_string(), 31);
+    let person = Person::new("Pratik", 31);
     println!("Welcome, {}!", person.name);
 
     let mut library = Library::new();
 
     // Add books to the library
-    library.add_book(Book::new(
-        "The Hobbit".to_string(),
-        "J.R.R. Tolkien".to_string(),
-    ));
-    library.add_book(Book::new(
-        "To Kill a Mockingbird".to_string(),
-        "Harper Lee".to_string(),
-    ));
-    library.add_book(Book::new("1984".to_string(), "George Orwell".to_string()));
+    library.add_book(Book::new("The Hobbit", "J.R.R. Tolkien"));
+    library.add_book(Book::new("To Kill a Mockingbird", "Harper Lee"));
+    library.add_book(Book::new("1984", "George Orwell"));
 
     // List the books in the library catalog
-    library.list_books();
+    library.list_all_available_books();
 
     // Borrow a book
-    let borrowed = library.borrow_book("1984");
+    let borrowed = library.borrow_book("1984", &person);
     if borrowed {
         println!("You've borrowed '1984'.");
     } else {
@@ -35,16 +29,18 @@ fn main() {
     }
 
     // List the updated library catalog
-    library.list_books();
+    library.list_all_available_books();
 
     // Return a book
     // Using Shadowing here.
-    let borrowed = library.borrow_book("1984");
+    let borrowed = library.borrow_book("1984", &person);
     if borrowed {
         println!("You've borrowed '1984'.");
     } else {
         println!("Sorry, '1984' is not available for borrowing.");
     }
+
+    library.list_all_checkout_books();
 
     let returned = library.return_book("1984");
     if returned {
@@ -54,5 +50,5 @@ fn main() {
     }
 
     // List the final library catalog
-    library.list_books();
+    library.list_all_available_books();
 }
